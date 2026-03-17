ac1ac594b658a47ea9555a79568d7cb3d2268281
OmPrakash05 <omprakashkhandale5@gmail.com>
2026-03-17 16:41:55 +0530

    Learn gsap animation fromto method

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import { useState } from 'react'
+import { useEffect } from 'react'
 import './App.css'
-import Hero from './component/Hero'
+// import Hero from './component/Hero'
+import gsap from 'gsap'
 
 function App() {
+  useEffect(()=> {
+    console.log("gsap running");
+    gsap.fromTo("h1", 
+    {opacity: 0},
+    {opacity:1 , duration: 2});
+  }, []);
   
 
   return (
-    <div className='bg-black text-white'>
-      <Hero/>
+    <div className='bg-black text-white h-screen flex items-center justify-center'>
+      <h1 className='text-4xl'>GSAP Start</h1>
       
     </div>
       )
--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,16 +1,34 @@
-import { useEffect } from "react";
+import { useLayoutEffect } from "react";
 import gsap from "gsap";
 
 const Hero = () => {
-    useEffect(() => {
-        gsap.from(".title", {
+    useLayoutEffect(() => {
+        const tl = gsap.timeline();
+
+        tl.from(".title", {
             y: -50,
             opacity: 0,
+            duration: 1
         })
+
+        .from(".subtitle",{
+            y: -50,
+            opacity: 0,
+            duration:1,
+            
+        })
+
+        .from(".btn", {
+            scale: 0,
+            duration: 0.5,
+            opacity: 0,
+            duration: 0.5
+            
+        });
         
-    })
+    }, []);
     return ( 
-        <div className="h-screen flex flex-col justify-center items-center gap-6">
+        <div className="h-screen flex flex-col justify-center items-center gap-6 bg-black text-white">
             <h1 className="title text-5xl font-bold">GSAP Landing</h1>
             <p className="subtitle text-lg text-gray-400">
                 Learn animations the right way.
