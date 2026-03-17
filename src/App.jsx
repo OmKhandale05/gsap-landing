1cfc5f030d9d85f430435687f873136721d55811
OmPrakash05 <omprakashkhandale5@gmail.com>
2026-03-17 16:45:46 +0530

    Add more elements to test animation

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,12 @@ function App() {
   
 
   return (
-    <div className='bg-black text-white h-screen flex items-center justify-center'>
-      <h1 className='text-4xl'>GSAP Start</h1>
+    <div className='bg-black text-white h-screen flex flex-col items-center gap-4 justify-center'>
+      <h1 className='title text-4xl'>GSAP Start</h1>
+      <p className='subtitle text-gray-400'>Learning step by step</p>
+      <button className='btn bg-white text-black px-4 py-2 rounded-lg '>
+        Start
+      </button>
       
     </div>
       )
