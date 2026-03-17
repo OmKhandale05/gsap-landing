2e2bca0cfd48046bdf7f8083a70ee3fc8af04b10
OmPrakash05 <omprakashkhandale5@gmail.com>
2026-03-17 17:17:39 +0530

    learn about scrolltrigger animation and apply it on product section with use of stagger

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,4 +1,25 @@
+import gsap from "gsap";
+import { useEffect } from "react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 const Product = () => {
+
+    useEffect(() => {
+        gsap.registerPlugin(ScrollTrigger);
+
+        gsap.fromTo(".card",
+        {opacity: 0, y: 100 },
+        {
+            opacity: 1,
+            y: 0,
+            duration: 1,
+            stagger: 0.3,
+            scrollTrigger: {
+                trigger: ".card",
+                start: "top 80%"
+            }
+        });
+    })
     return ( 
         <div className="min-h-screen bg-gray-900 text-white p-10">
             <h2 className="text-3xl mb-10 text-center">Products</h2>
