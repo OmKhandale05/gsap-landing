eaefa03d5ad27fc98046f9d37cff259107fce93f
OmPrakash05 <omprakashkhandale5@gmail.com>
2026-03-18 14:35:58 +0530

    Add marker lines and apply basic scroll stable

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -8,20 +8,27 @@ const Product = () => {
         gsap.registerPlugin(ScrollTrigger);
 
         gsap.fromTo(".card",
-        {opacity: 0, y: 100 },
+        {opacity: 0, y: 80 },
         {
             opacity: 1,
             y: 0,
             duration: 1,
             stagger: 0.3,
             scrollTrigger: {
-                trigger: ".card",
-                start: "top 80%"
-            }
+                trigger: ".cards-container",
+                start: "top 85%",
+                markers: true
+            },
         });
-    })
+
+    }, []);
     return ( 
-        <div className="min-h-screen bg-gray-900 text-white p-10">
+        <div className="bg-black text-white">
+            <div className="h-[150vh] flex items-center justify-center">
+                <h1 className="text-4xl">Scroll Down</h1>
+            </div>
+
+            <div className="cards-container min-h-screen p-10 bg-gray-900">
             <h2 className="text-3xl mb-10 text-center">Products</h2>
 
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
@@ -30,6 +37,10 @@ const Product = () => {
                 <div className="card bg-black p-6 rounded-xl">Card3</div>
             </div>
         </div>
+    
+
+        </div>
+        
      );
 }
  
